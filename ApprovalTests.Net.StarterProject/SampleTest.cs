0d96ae5e778d257226dd49c5273fb653dbc79d7c
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-11 19:56:04 +0000

    finished and refactored

--- ApprovalTests.Net.StarterProject/SampleTest.cs
+++ ApprovalTests.Net.StarterProject/SampleTest.cs
@@ -20,11 +20,31 @@ namespace ApprovalTests.Net.StarterProject
             var board = new []{new Point(1, 1)};
             var board2 = AdvanceTurn(board);
             Approvals.Verify(StringUtils.DisplayGrid(10,10, (x,y) => board2.Contains(new Point(x,y))? "x": "."));
+        }        
+        
+        [TestMethod]
+        public void TestThatASquareSurvives()
+        {
+            var board = new []{new Point(1, 1), new Point(1, 2) , new Point(2, 1) , new Point(2, 2) };
+            var board2 = AdvanceTurn(board);
+            Approvals.Verify(StringUtils.DisplayGrid(10,10, (x,y) => board2.Contains(new Point(x,y))? "x": "."));
         }
+
+
+        [TestMethod]
+        public void TestThatAGliderGlides()
+        {
+            var board = new[] { new Point(3, 3), new Point(4, 4), new Point(4, 5), new Point(5, 3), new Point(5,4) };
+            var board2 = AdvanceTurn(board);
+            Approvals.Verify(StringUtils.DisplayGrid(10, 10, (x, y) => board2.Contains(new Point(x, y)) ? "x" : "."));
+        }
+
 
         private IEnumerable<Point> AdvanceTurn(Point[] board)
         {
-            return board.SelectMany(p => GetNeighbours(p)).GroupBy(p => p);
+            IEnumerable<IGrouping<Point, Point>> counts = board.SelectMany(p => GetNeighbours(p)).GroupBy(p => p);
+            var alive = counts.Where(g => g.Count() == 3 || (g.Count() == 2 && board.Contains(g.Key))).Select(g => g.Key);
+            return alive;
         }
 
         private IEnumerable<Point> GetNeighbours(Point point)
