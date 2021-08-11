20b3d3759646cd798e417bfa05d05462886501bc
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-08-11 18:57:09 +0000

    at end

--- ApprovalTests.Net.StarterProject/SampleTest.cs
+++ ApprovalTests.Net.StarterProject/SampleTest.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using ApprovalTests.Core;
 using ApprovalTests.Reporters;
+using ApprovalUtilities.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ApprovalTests.Net.StarterProject
@@ -10,14 +15,29 @@ namespace ApprovalTests.Net.StarterProject
     public class SampleTest
     {
         [TestMethod]
-        public void TestStraightMsTest()
+        public void TestThatASingleLonelyCellDies()
         {
-            Assert.AreEqual(5,5);
+            var board = new []{new Point(1, 1)};
+            var board2 = AdvanceTurn(board);
+            Approvals.Verify(StringUtils.DisplayGrid(10,10, (x,y) => board2.Contains(new Point(x,y))? "x": "."));
         }
-        [TestMethod]
-        public void TestWithApprovalTests()
+
+        private IEnumerable<Point> AdvanceTurn(Point[] board)
         {
-            Approvals.Verify(Code.SampleText());
+            return board.SelectMany(p => GetNeighbours(p)).GroupBy(p => p);
+        }
+
+        private IEnumerable<Point> GetNeighbours(Point point)
+        {
+            yield return new Point(point.X - 1, point.Y - 1);
+            yield return new Point(point.X - 0, point.Y - 1); 
+            yield return new Point(point.X + 1 , point.Y - 1); 
+            yield return new Point(point.X - 1, point.Y - 0);
+            yield return new Point(point.X + 1 , point.Y - 0);
+            yield return new Point(point.X - 1, point.Y + 1);
+            yield return new Point(point.X - 0, point.Y + 1); 
+            yield return new Point(point.X + 1 , point.Y + 1); 
+
         }
     }
 }
